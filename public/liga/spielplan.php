08d5756798904717c224f3ba4ffe266ea989c51c
Largofin <pka.gross@web.de>
2021-09-27 14:39:49 +0200

    Dateinamen und -pfad angepasst

--- public/liga/spielplan.php
+++ public/liga/spielplan.php
@@ -15,7 +15,7 @@ Html::$content = "Der Spielplan für das Einradhockey-Turnier in " . $spielplan-
 include '../../templates/header.tmp.php';
 include '../../templates/spielplan/spielplan_titel.tmp.php';
 include '../../templates/spielplan/spielplan_teamliste.tmp.php'; // Teamliste
-include '../../templates/spielplan/spielplan_spiele.tmp.php'; // Spiele
+include '../../templates/spielplan/spielplan_spiele_jgj.tmp.php'; // Spiele
 include '../../templates/spielplan/spielplan_turniertabelle.tmp.php'; // Abschlusstabelle
 include '../../templates/spielplan/spielplan_direkter_vergleich.tmp.php'; // Direkter Vergleich Tabellen
 include '../../templates/footer.tmp.php';
