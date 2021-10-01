6cfa218134185d021aa5fdd631403c113a1c7f4b
Largofin <pka.gross@web.de>
2021-10-01 15:05:49 +0200

    Fehlerprüfung

--- public/liga/archiv_turnierliste.php
+++ public/liga/archiv_turnierliste.php
@@ -36,68 +36,76 @@ include '../../templates/header.tmp.php';
 <h1 class="w3-text-primary">Turnierdetails der Saison <?=$saison->get_saison_string()?></h1>
 
 <p>
-    <?=Html::link("archiv_turnierliste.php?saison=" . $saison->get_saison_id() . "#liste_liga" , "Liste der Ligaturniere" , false, "reorder")?><br>
-    <?=Html::link("archiv_turnierliste.php?saison=" . $saison->get_saison_id() . "#liste_final" , "Liste der Finalturniere" , false, "reorder")?><br>
-    <?=Html::link("archiv_turnierliste.php?saison=" . $saison->get_saison_id() . "#ergebnisse_liga" , "Ergebnisse der Ligaturniere" , false, "reorder")?><br>
-    <?=Html::link("archiv_turnierliste.php?saison=" . $saison->get_saison_id() . "#ergebnisse_final" , "Ergebnisse der Finalturniere" , false, "reorder")?>
+    <?=(empty($ligaturniere)) ? '' : Html::link("archiv_turnierliste.php?saison=" . $saison->get_saison_id() . "#liste_liga" , "Liste der Ligaturniere" , false, "reorder") . '<br>'?>
+    <?=(empty($abschlussturniere)) ? '' : Html::link("archiv_turnierliste.php?saison=" . $saison->get_saison_id() . "#liste_final" , "Liste der Finalturniere" , false, "reorder") . '<br>'?>
+    <?=(empty($ligaturniere)) ? '' : Html::link("archiv_turnierliste.php?saison=" . $saison->get_saison_id() . "#ergebnisse_liga" , "Ergebnisse der Ligaturniere" , false, "reorder") . '<br>'?>
+    <?=(empty($abschlussturniere)) ? '' : Html::link("archiv_turnierliste.php?saison=" . $saison->get_saison_id() . "#ergebnisse_final" , "Ergebnisse der Finalturniere" , false, "reorder")?>
 </p>
 
-<!-- Ligaturniere -->
-<h2 id="liste_liga" class="w3-text-secondary">Liste der Ligaturniere</h2>
-<div class="w3-responsive w3-card">
-    <table class="w3-table w3-striped">
-        <thead class="w3-primary">
+<?php if (!empty($ligaturniere)): ?>
+    <!-- Ligaturniere -->
+    <h2 id="liste_liga" class="w3-text-secondary">Liste der Ligaturniere</h2>
+    <div class="w3-responsive w3-card">
+        <table class="w3-table w3-striped">
+            <thead class="w3-primary">
+                <tr>
+                    <th><b>Datum</b></th>
+                    <th><b>Ort</b></th>
+                    <th><b>Art</b></th>
+                    <th><b>Block</b></th>
+                </tr>
+            </thead>
+        <?php foreach ($ligaturniere as $turnier): ?>
             <tr>
-                <th><b>Datum</b></th>
-                <th><b>Ort</b></th>
-                <th><b>Art</b></th>
-                <th><b>Block</b></th>
+                <td><?=strftime("%a", strtotime($turnier->get_datum()))?>, <?=strftime("%d.%m.", strtotime($turnier->get_datum()))?></a></td>
+                <td><?=Html::link('archiv_turnierliste.php?saison=' . $saison->get_saison_id() . '#'. $turnier->get_turnier_id(), $turnier->get_ort(), false)?></td>
+                <td><?=$turnier->get_art() == 'final' ? '--' : $turnier->get_art()?></td>
+                <td><?=$turnier->get_tblock()== 'final' ? 'FINALE' : $turnier->get_tblock() ?></td>
             </tr>
-        </thead>
-    <?php foreach ($ligaturniere as $turnier) {?>
-        <tr>
-            <td><?=strftime("%a", strtotime($turnier->get_datum()))?>, <?=strftime("%d.%m.", strtotime($turnier->get_datum()))?></a></td>
-            <td><?=Html::link('archiv_turnierliste.php?saison=' . $saison->get_saison_id() . '#'. $turnier->get_turnier_id(), $turnier->get_ort(), false)?></td>
-            <td><?=$turnier->get_art() == 'final' ? '--' : $turnier->get_art()?></td>
-            <td><?=$turnier->get_tblock()== 'final' ? 'FINALE' : $turnier->get_tblock() ?></td>
-        </tr>
-    <?php } ?>
-    </table>
-</div>
+        <?php endforeach; ?>
+        </table>
+    </div>
+<?php endif; ?>
 
-<!-- Abschlussturniere -->
-<h2 id="liste_final" class="w3-text-secondary">Liste der Finalturniere</h2>
-<div class="w3-responsive w3-card">
-    <table class="w3-table w3-striped">
-        <thead class="w3-primary">
+<?php if (!empty($abschlussturniere)): ?>
+    <!-- Abschlussturniere -->
+    <h2 id="liste_final" class="w3-text-secondary">Liste der Finalturniere</h2>
+    <div class="w3-responsive w3-card">
+        <table class="w3-table w3-striped">
+            <thead class="w3-primary">
+                <tr>
+                    <th><b>Datum</b></th>
+                    <th><b>Ort</b></th>
+                    <th><b>Name</b></th>
+                </tr>
+            </thead>
+        <?php foreach ($abschlussturniere as $turnier): ?>
             <tr>
-                <th><b>Datum</b></th>
-                <th><b>Ort</b></th>
-                <th><b>Block</b></th>
+                <td><?=strftime("%a", strtotime($turnier->get_datum()))?>, <?=strftime("%d.%m.", strtotime($turnier->get_datum()))?></a></td>
+                <td><?=Html::link('archiv_turnier.php?turnier_id='. $turnier->get_turnier_id(), $turnier->get_ort(), false)?></td>
+                <td><?=$turnier->get_tname() ?></td>
             </tr>
-        </thead>
-    <?php foreach ($abschlussturniere as $turnier) {?>
-        <tr>
-            <td><?=strftime("%a", strtotime($turnier->get_datum()))?>, <?=strftime("%d.%m.", strtotime($turnier->get_datum()))?></a></td>
-            <td><?=Html::link('archiv_turnier.php?turnier_id='. $turnier->get_turnier_id(), $turnier->get_ort(), false)?></td>
-            <td><?=$turnier->get_tblock() ?></td>
-        </tr>
-    <?php } ?>
-    </table>
-</div>
+        <?php endforeach; ?>
+        </table>
+    </div>
+<?php endif; ?>
 
-<!-- Ergebnisse Ligaturniere -->
-<h2 id="ergebnisse_liga" class="w3-text-secondary">Ergebnisse der Ligaturniere</h2>
-<?php 
-    foreach ($ligaturniere as $turnier) {
-        $turnier->show();
-    }
-?>
+<?php if (!empty($ligaturniere)): ?>
+    <!-- Ergebnisse Ligaturniere -->
+    <h2 id="ergebnisse_liga" class="w3-text-secondary">Ergebnisse der Ligaturniere</h2>
+    <?php 
+        foreach ($ligaturniere as $turnier):
+            $turnier->show();
+        endforeach;
+    ?>
+<?php endif; ?>
 
-<!-- Ergebnisse Abschlussturniere -->
-<h2 id="ergebnisse_final" class="w3-text-secondary">Ergebnisse der Finalturniere</h2>
-<?php 
-    foreach ($abschlussturniere as $turnier) {
-        $turnier->show();
-    }
-?>
+<?php if (!empty($abschlussturniere)): ?>
+    <!-- Ergebnisse Abschlussturniere -->
+    <h2 id="ergebnisse_final" class="w3-text-secondary">Ergebnisse der Finalturniere</h2>
+    <?php 
+        foreach ($abschlussturniere as $turnier):
+            $turnier->show();
+        endforeach;
+    ?>
+<?php endif; ?>
